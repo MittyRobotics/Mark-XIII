5f6e7ea57640c5646b350e6b3ef84c8a349ff497
Peri Cabrales <pericabrales17@mittymonarch.com>
2016-01-12 17:16:28 -0800

    Added Comments and set talon control mode for speed

--- MarkXIII_dev/src/org/usfirst/frc/team1351/robot/evom/TKOFlywheel.java
+++ MarkXIII_dev/src/org/usfirst/frc/team1351/robot/evom/TKOFlywheel.java
@@ -6,7 +6,9 @@ import edu.wpi.first.wpilibj.Joystick;
 public class TKOFlywheel implements Runnable {
 	CANTalon flyTalon1 = new CANTalon(1);
 	Joystick stick1 = new Joystick(1), stick2 = new Joystick(2);
-	
+	//figure out what is wrong with this
 	flyTalon1.setSafetyEnabled(true);
-	CANTalon.
+	//assume that speed based on RPM
+	CANTalon.TalonControlMode Speed;
+	
 }
