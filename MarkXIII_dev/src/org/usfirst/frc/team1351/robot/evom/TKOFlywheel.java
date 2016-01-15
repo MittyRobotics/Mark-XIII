28bb65b2c8d25f3186089888439f1ca0d6199c7f
Peri Cabrales <pericabrales17@mittymonarch.com>
2016-01-14 17:23:42 -0800

    Removed the drive station and resolved problems with the encoder

--- MarkXIII_dev/src/org/usfirst/frc/team1351/robot/evom/TKOFlywheel.java
+++ MarkXIII_dev/src/org/usfirst/frc/team1351/robot/evom/TKOFlywheel.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1351.robot.evom;
 
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CounterBase;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
@@ -9,34 +10,29 @@ import edu.wpi.first.wpilibj.PIDController;
 public class TKOFlywheel implements Runnable {
 	CANTalon flyTalon1 = new CANTalon(1);
 	Joystick stick1 = new Joystick(1), stick2 = new Joystick(2);
-	//TODO: fix encoder
-	Encoder encoder = new Encoder(1, 2);
+	
+	//dummy numbers. fix when we know what the ports are going to be
+	Encoder encoder = new Encoder(1, 2, false, CounterBase.EncodingType.k4X);
 	//PID
 	float p = 0;
 	float i = 0;
 	float d = 0;
 	PIDController controller = new PIDController(p, i, d, encoder, flyTalon1);
 	double PIDsetpoint;
-	//TODO: understand why driver station isnt visible
-	DriverStation station = new DriverStation();
+	
+	//TODO: understand why driver station isn't visible
 	
 	
-	//TODO: figure out what is wrong with this
+	//TODO: Figure out what is wrong with this
 	flyTalon1.setSafetyEnabled(true);
 	//assume that speed based on RPM
 	CANTalon.TalonControlMode Speed;
 	
+	
 	//Increases the speed of the talon to ~9000 RPM(maybe)
 	public void increaseSpeed() {
 		flyTalon1.set(9000);
-	//TODO: Figure out why this bracket has an error
-	}
-	if(stick1.getTrigger(kRight)) {
-		increaseSpeed();
+		
 	}
 	
-	if()
-	
-	
-}
 }
