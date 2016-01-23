58a0ed68f5d54f31c9ba070bd32d5c40536fc984
Ben <bumjinkim16@mittymonarch.com>
2016-01-23 11:31:25 -0800

    Creating state machine branch

---
+++ MarkXIII_dev/src/org/usfirst/frc/team1351/robot/statemachine/states/ErrorState.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team1351.robot.statemachine.states;
+
+import org.usfirst.frc.team1351.robot.statemachine.IStateFunction;
+import org.usfirst.frc.team1351.robot.statemachine.InstanceData;
+import org.usfirst.frc.team1351.robot.statemachine.StateEnum;
+
+public class ErrorState implements IStateFunction
+{
+
+	@Override
+	public StateEnum doState(InstanceData data)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
