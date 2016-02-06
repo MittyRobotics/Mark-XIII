861bc73b529e903c92cd9660f7940ee4e568735e
Peri Cabrales <pericabrales17@mittymonarch.com>
2016-02-06 11:05:19 -0800

    Put in drive code for creeping

--- MarkXIII_dev/src/org/usfirst/frc/team1351/robot/evom/TKOArm.java
+++ MarkXIII_dev/src/org/usfirst/frc/team1351/robot/evom/TKOArm.java
@@ -1,70 +1,99 @@
 package org.usfirst.frc.team1351.robot.evom;
 
 import org.usfirst.frc.team1351.robot.drive.TKODrive;
+import org.usfirst.frc.team1351.robot.logger.TKOLogger;
 import org.usfirst.frc.team1351.robot.main.Definitions;
 import org.usfirst.frc.team1351.robot.util.TKOException;
 import org.usfirst.frc.team1351.robot.util.TKOHardware;
 import org.usfirst.frc.team1351.robot.util.TKOThread;
 import org.usfirst.frc.team1351.robot.util.ThreadExample;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class TKOArm implements Runnable{
+public class TKOArm implements Runnable
+{
 
-	public TKOArm() {
-		
+	public TKOArm()
+	{
+
 	}
-	
+
 	boolean armUp = false;
 	public TKOThread exampleThread = null;
 	private TKOArm m_Instance = null;
 	double driveSetpoint = 0;
-	
-	void armMove() throws TKOException {
-		if(armUp == false) {
+
+	void armMove() throws TKOException
+	{
+		if (armUp == false)
+		{
 			TKOHardware.getDSolenoid(1).set(Value.kReverse);
 			armUp = true;
-		} else {
+		}
+		else
+		{
 			TKOHardware.getDSolenoid(1).set(Value.kForward);
 			armUp = false;
 		}
 	}
-	
-	
+
 	double p, i, d, distance, incrementer, threshold;
-	void TKOCreep() throws TKOException {
-		//3.183 revs needed (~40")
-		//TODO figure out how to make it creep (use DriveAtom somehow?)
-		TKODrive.getInstance().setCreepAtomRunning(true); 
-		
+
+	void TKOCreep() throws TKOException
+	{
+		// 3.183 revs needed (~40")
+		// TODO figure out how to make it creep (use DriveAtom somehow?)
+		TKODrive.getInstance().setCreepAtomRunning(true);
+
 		p = SmartDashboard.getNumber("Drive P: ");
 		i = SmartDashboard.getNumber("Drive I: ");
 		d = SmartDashboard.getNumber("Drive D: ");
-		distance = -795.75; 
+		// arms on the back
+		distance = -795.75;
 		incrementer = Definitions.DRIVE_ATOM_INCREMENTER;
 		threshold = 75; // we can be within approx. half an inch
 		TKOHardware.autonInit(p, i, d);
 		System.out.println("Starting Creep Stuff");
-		
-		
-		TKODrive.getInstance().setCreepAtomRunning(false); 
+		try
+		{
+			while (DriverStation.getInstance().isEnabled() && TKOHardware.getLeftDrive().getSetpoint() > distance)
+			{
+				TKOHardware.getLeftDrive().set(TKOHardware.getLeftDrive().getSetpoint() - incrementer);
+				TKOHardware.getRightDrive().set(TKOHardware.getRightDrive().getSetpoint() - incrementer);
+
+				System.out.println("Encoder Left: " + TKOHardware.getLeftDrive().getPosition() + "\t Encoder Right: "
+						+ TKOHardware.getRightDrive().getPosition() + "\t Left Setpoint: " + TKOHardware.getLeftDrive().getSetpoint());
+				TKOLogger.getInstance().addMessage("Encoder Left: " + TKOHardware.getLeftDrive().getPosition() + "\t Encoder Right: "
+						+ TKOHardware.getRightDrive().getPosition() + "\t Left Setpoint: " + TKOHardware.getLeftDrive().getSetpoint());
+				Timer.delay(0.001);
+			}
+			TKODrive.getInstance().setCreepAtomRunning(false);
+		}
+		catch (TKOException e)
+		{
+			e.printStackTrace();
+		}
 	}
-	
-	void TKOPortcullis () throws TKOException {
+
+	void TKOPortcullis() throws TKOException
+	{
 		armMove();
 		TKOCreep();
 		armMove();
 	}
-	
-	void TKODrawbridge() throws TKOException {
+
+	void TKODrawbridge() throws TKOException
+	{
 		armMove();
 		TKOCreep();
 		armMove();
 		TKOCreep();
 	}
-	
+
 	public synchronized TKOArm getInstance()
 	{
 		if (m_Instance == null)
@@ -76,19 +105,19 @@ public class TKOArm implements Runnable{
 	}
 
 	/**
-	 * @throws TKOException 
-	 * The {@code start} method starts the thread, making it call the run method (only once) but can do this for threads in different
-	 * classes in parallel. The {@code isThreadRunning} method checks with a boolean whether the thread is running. We only start the thread
-	 * if it is not. The {@code setThreadRunning} method sets the boolean to true, and the {@code start} method starts the Thread. We use
-	 * the {@code isThreadRunning} in the run function to verify whether our thread should be running or not, to make a safe way to stop the
-	 * thread. This function is completely thread safe.
+	 * @throws TKOException The {@code start} method starts the thread, making it call the run method (only once) but can do this for
+	 * threads in different classes in parallel. The {@code isThreadRunning} method checks with a boolean whether the thread is running. We
+	 * only start the thread if it is not. The {@code setThreadRunning} method sets the boolean to true, and the {@code start} method starts
+	 * the Thread. We use the {@code isThreadRunning} in the run function to verify whether our thread should be running or not, to make a
+	 * safe way to stop the thread. This function is completely thread safe.
 	 * 
 	 * @category
-	 
+	 * 
 	 */
 	public void start() throws TKOException
 	{
-		TKOHardware.changeTalonMode(TKOHardware.getDriveTalon(0), TalonControlMode.PercentVbus, Definitions.AUTON_DRIVE_P, Definitions.AUTON_DRIVE_I, Definitions.AUTON_DRIVE_D);
+		TKOHardware.changeTalonMode(TKOHardware.getDriveTalon(0), TalonControlMode.PercentVbus, Definitions.AUTON_DRIVE_P,
+				Definitions.AUTON_DRIVE_I, Definitions.AUTON_DRIVE_D);
 		if (!exampleThread.isAlive() && m_Instance != null)
 		{
 			exampleThread = new TKOThread(m_Instance);
@@ -130,11 +159,12 @@ public class TKOArm implements Runnable{
 				synchronized (exampleThread) // synchronized per the thread to make sure that we wait safely
 				{
 					exampleThread.wait(100); // the wait time that the thread sleeps, in milliseconds
-					}
 				}
-			} catch (Exception e)
-			{
-				e.printStackTrace();
 			}
 		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
+}
