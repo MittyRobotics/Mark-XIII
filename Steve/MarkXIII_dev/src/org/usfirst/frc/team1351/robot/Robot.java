4859155405898b53f7cbd286a46c6c12c1e47cdb
Stephen Hunt <stephenhunt20@mittymonarch.com>
2016-11-29 15:44:41 -0800

    I finished fixing my life.

--- Steve/MarkXIII_dev/src/org/usfirst/frc/team1351/robot/Robot.java
+++ Steve/MarkXIII_dev/src/org/usfirst/frc/team1351/robot/Robot.java
@@ -15,18 +15,15 @@ package org.usfirst.frc.team1351.robot;
 
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Timer;
+//import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 
 public class Robot extends SampleRobot {
-	public CANTalon Wheel0,Wheel1,Wheel2,Wheel3,Crate0,Crate1;
-	Joystick joystick0, joystick1;
-	static double deadzone = 0.05;
-    double y0 = 0;
-    double y1 = 0;
-    public double currentLevel = 0;
-    public double stepLength = 10;
+	public CANTalon Wheel0,Wheel1,Wheel2,Wheel3;
+	Joystick joystick0;
+    public double degPerFt = 171.888;
+    public double deg90 = 100;
 
     //Defining everything
     //deadzone is the plus/minus value to create a deadzone of 5% for the joystick
@@ -36,16 +33,14 @@ public class Robot extends SampleRobot {
         Wheel1 = new CANTalon(1);
         Wheel2 = new CANTalon(2);
         Wheel3 = new CANTalon(3);
-        Crate0 = new CANTalon(4);
-        Crate1 = new CANTalon(5);
-        joystick0 = new Joystick(0);
-        joystick1 = new Joystick(1);
         Wheel0.changeControlMode(TalonControlMode.PercentVbus);
         Wheel1.changeControlMode(TalonControlMode.PercentVbus);
         Wheel2.changeControlMode(TalonControlMode.PercentVbus);
         Wheel3.changeControlMode(TalonControlMode.PercentVbus);
-        Crate0.changeControlMode(TalonControlMode.PercentVbus);
-        Crate1.changeControlMode(TalonControlMode.PercentVbus);
+        Wheel0.setSafetyEnabled(true);
+    	Wheel1.setSafetyEnabled(true);
+    	Wheel2.setSafetyEnabled(true);
+    	Wheel3.setSafetyEnabled(true);
     }
     
     public void robotInit() {
@@ -53,87 +48,59 @@ public class Robot extends SampleRobot {
     }
     
     public void autonomous() {
+    	while (isEnabled()) {
+        	Wheel2.setEncPosition(0);
+        	Wheel0.set(.4);
+    		Wheel1.set(.4);
+    		Wheel2.set(-.4);
+    		Wheel3.set(-.4);
+    		while(Wheel2.getEncPosition() < degPerFt * 60) {
+    			Wheel0.set(.4);
+        		Wheel1.set(.4);
+        		Wheel2.set(-.4);
+        		Wheel3.set(-.4);
+    		}
+    		Wheel0.set(0);
+    		Wheel1.set(0);
+    		Wheel2.set(0);
+    		Wheel3.set(0);
+    		
+    		break;
+    		
+//    		Wheel2.setEncPosition(0);
+//    		Wheel0.set(0.1);
+//    		Wheel1.set(0.1);
+//    		Wheel2.set(0.1);
+//    		Wheel3.set(0.1);
+//    		while(Wheel2.getEncPosition() < deg90){ 
+//
+//    		}
+//    		Wheel0.set(0);
+//    		Wheel1.set(0);
+//    		
+//    		Wheel2.set(0);
+//    		Wheel3.set(0);
+//    		Wheel2.setEncPosition(0);
+//        	Wheel0.set(-0.1);
+//    		Wheel1.set(-0.1);
+//    		Wheel2.set(0.1);
+//    		Wheel3.set(0.1);
+//    		while(Wheel2.getEncPosition() < degPerFt * 5){ 
+//    		
+//    		}
+//    		Wheel0.set(0);
+//    		Wheel1.set(0);
+//    		Wheel2.set(0);
+//    		Wheel3.set(0);
+    	}
+    	Wheel0.set(0);
+    	Wheel1.set(0);
+    	Wheel2.set(0);
+    	Wheel3.set(0);
     	
     }
 
     public void operatorControl() {
-    	Wheel0.setSafetyEnabled(true);
-    	Wheel1.setSafetyEnabled(true);
-    	Wheel2.setSafetyEnabled(true);
-    	Wheel3.setSafetyEnabled(true);
-    	Crate0.setSafetyEnabled(true);
-    	Crate1.setSafetyEnabled(true);
-    	
-    	//turns on safety for motors
-    	while (isOperatorControl() && isEnabled()) {
-    		if (joystick0.getY() >-deadzone&&joystick0.getY() <deadzone){
-        		y0 = 0;
-        	}
-    		else {
-    			y0 = joystick0.getY();
-    	    	Wheel2.set(y0);
-    	    	Wheel3.set(y0);
-    	    	//sets the motors to run at the percent that the left joystick is pushed forward
-    		}
-    		
-    		if (joystick1.getY() >-deadzone&&joystick1.getY() <deadzone){
-    			y1 = 0;
-    		} 
-    		else {
-    	    	y1 = joystick1.getY();
-        		Wheel0.set(-y1);
-        		Wheel1.set(-y1);
-        		//sets the motors to run at the percent that the right joystick is pushed forward
-    		}
-    		if (joystick0.getRawButton(1)) {
-    			if(currentLevel == 5){
-        			System.out.println("Out of bounds");
-        		}
-    			else if(currentLevel < 5) {
-        			Crate0.set(0.1);
-        			Crate1.set(0.1);
-        			//turns on crate motors
-        			while(Wheel1.getEncPosition() < (currentLevel + 1) * stepLength){ //Check
-        				if (joystick0.getRawButton(3)) {
-        					break;
-        					//safety force quit
-        				}
-        			}
-        			Crate0.set(0);
-        			Crate1.set(0);
-        			currentLevel++;
-        			Timer.delay(1);
-        			//delay between checking joystick
-        		}
-    		} 
-    		else if (joystick0.getRawButton(2)) {
-    			if(currentLevel == 0){
-    				System.out.println("Out of bounds");
-    			}
-    			else if(currentLevel > 0) {
-    				Crate0.set(-0.1);
-    				Crate1.set(-0.1);
-    				while(Wheel1.getEncPosition() > (currentLevel - 1) * stepLength){ //Check
-    					if (joystick0.getRawButton(3)) {
-    						break;
-    					}
-    				}
-    				//moves the motors for picking up crates
-    				Crate0.set(0);
-    				Crate1.set(0);
-    				currentLevel--;
-    				Timer.delay(1);
-    				//delay
-    			}
-    		}
-    		Timer.delay(0.005);
-    		//waits for a little bit between reading the joysticks and changing the motor power
-    		
-    	}
-    	
-    		
-    	
     	
     }
-
 }
